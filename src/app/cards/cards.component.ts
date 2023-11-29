59e35d5fcdb78e4d7037c4f5815e0008ea8913fd
Steven <villarst@mail.gvsu.edu>
2023-11-29 00:19:24 -0500

    Got the buttons and filters to work. They search for cards corresponding to the checkbox clicked.

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PokemonService } from 'src/app/pokemon.service';
 
+
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
@@ -11,29 +12,88 @@ export class CardsComponent {
   title = 'Pokemon Cards Page.';
   cardInfos: any[] = []; // Initialize as an empty array to store fetched card data
 
-  // name_age = {
-  //   'sahil': 25,
-  //   'anaya': 8,
-  //   'atul': 16
-  // };
-
+  cards_in_sets = {
+    'swsh-1': 216,
+    'swsh-2': 209
+  };
+  
   totalCards = 11; // Total number of cards to fetch
 
-  constructor(private pokemonService: PokemonService) { }
+  // Add properties to track checkbox states
+  darknessAblazeChecked: boolean = false;
+  battleStylesChecked: boolean = false;
+
+  constructor(private pokemonService: PokemonService) { 
+    let map = new Map<string, string[]>();
+    map.set('Darkness Ablaze', ['swsh-1']);
+    map.get('Darkness Ablaze')?.push('216'); // Adding 'value2' to the array
+    
+    map.set('Battle Styles', ['swsh-2']);
+    map.get('Battle Styles')?.push('209');
+  }
 
   async logMessage() {
     console.log('logMessage() called');
-
     this.cardInfos = []; // Clear the previous data before fetching new cards
+  
+    // Apply checkbox states before fetching cards
+    if (this.darknessAblazeChecked) {
+      this.totalCards = this.cards_in_sets['swsh-1'];
+      for (let i = 1; i <= this.totalCards; i++) {
+        const cardId = 'swsh1-' + i;
+        try {
+          const data = await this.pokemonService.showTestCard(cardId).toPromise();
+          this.cardInfos.push(data); // Push the fetched card data into the array
+          console.log('Fetched card:', cardId);
+        } catch (error) {
+          console.error('Error fetching card:', cardId, error);
+        }
+      }
+    } 
+    else if (this.battleStylesChecked){
+      this.totalCards = this.cards_in_sets['swsh-2'];
+      for (let i = 1; i <= this.totalCards; i++) {
+        const cardId = 'swsh2-' + i;
+        try {
+          const data = await this.pokemonService.showTestCard(cardId).toPromise();
+          this.cardInfos.push(data); // Push the fetched card data into the array
+          console.log('Fetched card:', cardId);
+        } catch (error) {
+          console.error('Error fetching card:', cardId, error);
+        }
+      }
+    }
+    else{
+      this.totalCards = 200;
+      for (let i = 1; i <= this.totalCards; i++) {
+        const cardId = 'swsh3-' + i;
+        try {
+          const data = await this.pokemonService.showTestCard(cardId).toPromise();
+          this.cardInfos.push(data); // Push the fetched card data into the array
+          console.log('Fetched card:', cardId);
+        } catch (error) {
+          console.error('Error fetching card:', cardId, error);
+        }
+      }
+    }
+  }
 
-    for (let i = 1; i <= this.totalCards; i++) {
-      const cardId = 'swsh3-' + i;
-      try {
-        const data = await this.pokemonService.showTestCard(cardId).toPromise();
-        this.cardInfos.push(data); // Push the fetched card data into the array
-        console.log('Fetched card:', cardId);
-      } catch (error) {
-        console.error('Error fetching card:', cardId, error);
+  onCheckboxChange(event: any, checkbox: string) {
+    if (checkbox === 'darknessAblaze') {
+      this.darknessAblazeChecked = event.target.checked;
+      if (this.darknessAblazeChecked) {
+        this.totalCards = this.cards_in_sets['swsh-1']; // Set totalCards to 216 when Darkness Ablaze is checked
+      } else {
+        // Set totalCards to the default value or any other desired value when unchecked
+        this.totalCards = 11;
+      }
+    } else if (checkbox === 'battleStyles') {
+      this.battleStylesChecked = event.target.checked;
+      if (this.battleStylesChecked) {
+        this.totalCards = this.cards_in_sets['swsh-2']; // Set totalCards to 216 when Darkness Ablaze is checked
+      } else {
+        // Set totalCards to the default value or any other desired value when unchecked
+        this.totalCards = 11;
       }
     }
   }
